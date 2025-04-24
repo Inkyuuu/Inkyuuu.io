2d70c2a4f63ba5a835cd2c28602f2e94f2f4a1d4
Inkyuu <amyzeng77@gmail.com>
2025-04-24 16:23:24 -0700

    resume changes

--- src/Resume/Resume.tsx
+++ src/Resume/Resume.tsx
@@ -1,10 +1,10 @@
-
+import resume from '../../src/assets/Resume 2025.pdf'
 function Resume(){
     return(
       <center>
         <iframe
-        src="https://docs.google.com/document/d/e/2PACX-1vS-dY-pg89hglvyp9FGGgja-ADSqlaWwcTvxfzcPAOzaBUqtWmLr9M6a9zNoV5g-QgfCzLORUNcH1U6/pub?embedded=true"
-        style={{ width: "816px", height: "1082px", border: "none" }}
+        src={resume}
+        style={{ width: "100%", height: "1082px", border: "none" }}
         >
         </iframe>
       </center>
