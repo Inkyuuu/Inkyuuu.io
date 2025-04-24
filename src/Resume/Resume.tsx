0646bd098362112bf9686d894ed4c8723e9c3a6b
Inkyuu <amyzeng77@gmail.com>
2025-04-24 08:25:57 -0700

    centered resume w scrollbar

--- src/Resume/Resume.tsx
+++ src/Resume/Resume.tsx
@@ -1,16 +1,13 @@
 
 function Resume(){
     return(
-      <div className="main">
-        <center>
-          <iframe
-          loading="lazy"
-          src="https://docs.google.com/document/d/e/2PACX-1vS-dY-pg89hglvyp9FGGgja-ADSqlaWwcTvxfzcPAOzaBUqtWmLr9M6a9zNoV5g-QgfCzLORUNcH1U6/pub?embedded=true"
-          style={{ width: "70%", height: "1200px", border: "none" }}
-          >
-          </iframe>
-        </center>
-      </div>
+      <center>
+        <iframe
+        src="https://docs.google.com/document/d/e/2PACX-1vS-dY-pg89hglvyp9FGGgja-ADSqlaWwcTvxfzcPAOzaBUqtWmLr9M6a9zNoV5g-QgfCzLORUNcH1U6/pub?embedded=true"
+        style={{ width: "816px", height: "1082px", border: "none" }}
+        >
+        </iframe>
+      </center>
       
     );
 }
